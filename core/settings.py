13260630c3dad1032c7d80853447b6b19140aa3e
website-krysta <website.krysta@gmail.com>
2025-07-04 10:00:47 +0530

    update changes

--- core/settings.py
+++ core/settings.py
@@ -147,7 +147,7 @@ if DEBUG == True:
 )
 
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATICFILES_DIRS = (
