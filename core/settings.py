09c16f025a878c832340ab92cdefe58a20d4adb4
website-krysta <website.krysta@gmail.com>
2025-07-04 10:36:21 +0530

    fixed issues

--- core/settings.py
+++ core/settings.py
@@ -147,7 +147,7 @@ if DEBUG == True:
 )
 
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATICFILES_DIRS = (
